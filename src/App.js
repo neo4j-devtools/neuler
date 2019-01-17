fc28526324a4a20f7b39dc7476b142b2e2430e34
Irfan Nuri Karaca <irfannuri@gmail.com>
2019-01-17 10:14:57 +0000

    Setup create react app and initial view

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Sidebar, Menu, Segment, Icon, Image, Header } from "semantic-ui-react"
+import './App.css'
+
+class App extends Component {
+  state = {
+    content: 'centralities'
+  }
+
+  handleMenuClick (content) {
+    this.setState({content})
+  }
+
+  render() {
+    const {content} = this.state
+    const contentComponent = 'HELLO'
+
+    return (
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar
+          as={Menu}
+          animation='push'
+          direction='left'
+          icon='labeled'
+          inverted
+          vertical
+          visible={true}
+          width='thin'
+        >
+          <Menu.Item active={content === 'centralities'} as='a' onClick={this.handleMenuClick.bind(this, 'centralities')}>
+            <Icon name='sun' />
+            Centralities
+          </Menu.Item>
+          <Menu.Item active={content === 'community'} as='a' onClick={this.handleMenuClick.bind(this, 'community')}>
+            <Icon name='group' />
+            Community Detection
+          </Menu.Item>
+          <Menu.Item as='a'>
+            <Icon name='connectdevelop' />
+            Path Finding
+          </Menu.Item>
+          <Menu.Item as='a'>
+            <Icon name='clone' />
+            Similarities
+          </Menu.Item>
+        </Sidebar>
+
+        <Sidebar.Pusher>
+          <Segment basic inverted vertical={false} style={{height: '5em'}}>
+            <Header as='h1' inverted color='grey'>
+              NEuler - Playground for Neo4j Graph Algorithms
+            </Header>
+          </Segment>
+         {contentComponent}
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    )
+  }
+}
+
+export default App
