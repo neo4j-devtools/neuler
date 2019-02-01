5a16478d0bc104ba75b5f6caee695c793401807a
Irfan Nuri Karaca <irfannuri@gmail.com>
2019-02-02 00:27:19 +0300

    Make run results tab a part of algorithm result header

--- src/components/Centralities/AlgoResults.js
+++ src/components/Centralities/AlgoResults.js
@@ -25,8 +25,8 @@ const getAlgoPanes = task => [{
                 <Segment>:param {key} =>
                   {task.parameters[key]
                     ? (typeof task.parameters[key] === 'string'
-                        ? ` '${task.parameters[key]}'`
-                        : ` ${task.parameters[key]}`)
+                      ? ` '${task.parameters[key]}'`
+                      : ` ${task.parameters[key]}`)
                     : ' null'};
                 </Segment>
               )}
@@ -36,31 +36,75 @@ const getAlgoPanes = task => [{
 
       <Segment>{task.query}</Segment>
     </div>
-    )
+  )
 }, {
   menuItem: `Vis`,
-  render: () => <GraphVisualiser taskId={task.taskId} results={task.result} label={task.parameters.label} relationshipType={task.parameters.relationshipType} writeProperty={task.parameters.writeProperty} />
+  render: () => <div style={tabContentStyle}>
+    <GraphVisualiser taskId={task.taskId} results={task.result} label={task.parameters.label}
+                     relationshipType={task.parameters.relationshipType}
+                     writeProperty={task.parameters.writeProperty}/>
+  </div>
 }]
 
-const getResultPanes = tasks => tasks.map(task =>
-  ({
-    menuItem: `${task.algorithm}. Started at: ${task.startTime.toLocaleString()}`,
-    render: () => <HorizontalAlgoTab task={task} key={task.id}/>
-  })
-)
-
-const TabExampleVerticalTabular = ({ tasks }) => (
-  <div style={{ width: '95%' }}>
-    <Header as='h2'>Run Results</Header>
-    <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={getResultPanes(tasks)}/>
+const HorizontalAlgoTab = ({ task, prevResult, nextResult, currentPage, totalPages }) => (
+  <div style={{ paddingTop: '1em' }}>
+    <Tab menu={{ vertical: false, tabular: true }} panes={getAlgoPanes(task)}/>
+    <div style={{
+      position: 'absolute',
+      top: '1.5em',
+      left: '30em',
+      display: 'flex',
+      alignItems: 'center'
+    }}>
+      <Button basic icon size='mini' onClick={prevResult} disabled={currentPage === 1}>
+        <Icon name='angle left'/>
+      </Button>
+      <Header as='h3' style={{ margin: '0 1em' }}>
+        {`${task.algorithm} Started at: ${task.startTime.toLocaleTimeString()} - (${currentPage} / ${totalPages})`}
+      </Header>
+      <Button basic icon size='mini' onClick={nextResult} disabled={currentPage === totalPages}>
+        <Icon name='angle right'/>
+      </Button>
+    </div>
   </div>
 )
 
-const HorizontalAlgoTab = ({ task }) => (
-  <div style={{ width: '95%' }}>
-    <Tab menu={{ fluid: true, vertical: false, tabular: true }} panes={getAlgoPanes(task)}/>
-  </div>
-)
+class TabExampleVerticalTabular extends Component {
+  state = {
+    page: 0
+  }
+
+  prevResult() {
+    this.setState(({ page }) => ({ page: Math.max(0, page - 1) }))
+  }
+
+  nextResult() {
+    const length = (this.props.tasks || []).length
+    this.setState(({ page }) => ({ page: Math.min(length - 1, page + 1) }))
+  }
+
+  render() {
+    const tasks = this.props.tasks
+    const page = this.state.page
+    if (tasks && tasks.length > 0) {
+      const currentTask = tasks[this.state.page]
+      return <HorizontalAlgoTab
+        task={currentTask}
+        prevResult={this.prevResult.bind(this)}
+        nextResult={this.nextResult.bind(this)}
+        currentPage={page + 1}
+        totalPages={tasks.length}
+      />
+    } else {
+      return null
+    }
+    /*(
+      <div style={{  }}>
+        <Tab menu={{ vertical: false, tabular: true }} panes={getResultPanes(tasks)}/>
+      </div>
+    )*/
+  }
+}
 
 
 const mapStateProps = state => ({
