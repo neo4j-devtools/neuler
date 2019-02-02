eae0251208ee7104d792c5d1ea6d5eb8d861dd33
Irfan Nuri Karaca <irfannuri@gmail.com>
2019-02-02 21:56:15 +0300

    Manage active group and algorithm via redux

---
+++ src/ducks/algorithms.js
@@ -0,0 +1,42 @@
+import { getAlgorithmDefinitions, getAlgorithms } from "../components/algorithmsLibrary"
+
+const NAME = 'ALGORITHMS'
+const SELECT_GROUP = `${NAME}/SELECT_GROUP`
+const SELECT_ALGORITHM = `${NAME}/SELECT_ALGORITHM`
+
+export const selectGroup = group => ({
+  type: SELECT_GROUP,
+  group
+})
+
+export const selectAlgorithm = algorithm => ({
+  type: SELECT_ALGORITHM,
+  algorithm
+})
+
+const initialState = {
+  group: 'Centralities',
+  algorithm: 'Page Rank'
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SELECT_GROUP:
+      return {
+        algorithm: getAlgorithms(action.group)[0],
+        group: action.group
+      }
+    case SELECT_ALGORITHM:
+      return {
+        ...state,
+        algorithm: action.algorithm
+      }
+    default:
+      return state
+  }
+}
+
+export const getCurrentAlgorithm = state => {
+  const { group, algorithm } = state.algorithms
+  return getAlgorithmDefinitions(group, algorithm)
+}
--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import tasks from './tasks'
+import algorithms from './algorithms'
 
 const reducers = {
+  algorithms,
   tasks
 }
 
