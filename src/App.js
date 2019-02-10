dd7d414675846b3c3d9d7929c8ea432c68eb9ca9
Mark Needham <m.h.needham@gmail.com>
2019-02-10 22:41:00 +0000

    NEuler as its own component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 
 import AlgorithmsGroupMenu from "./components/AlgorithmGroupsMenu"
 import CheckGraphAlgorithmsInstalled from "./components/CheckGraphAlgorithmsInstalled"
+import NEuler from "./components/NEuler"
 import { selectAlgorithm } from "./ducks/algorithms"
 import { connect } from "react-redux"
 import { getAlgorithms } from "./components/algorithmsLibrary"
@@ -19,46 +20,6 @@ import { setConnected, setDisconnected, CONNECTED, CONNECTING, DISCONNECTED, INI
 import { initializeConnection, tryConnect } from "./services/connections"
 
 
-class NEuler extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  state = {
-    status: 'groups',
-    content: 'centralities'
-  }
-
-  handleMenuClick (content) {
-    this.setState({content})
-  }
-
-  render() {
-    const { activeGroup, activeAlgorithm, selectAlgorithm } = this.props
-
-    return (
-      <Container fluid style={{ display: 'flex' }}>
-          <AlgorithmsGroupMenu/>
-        <div style={{width: '100%'}}>
-          <Segment basic inverted vertical={false}
-                   style={{ height: '5em', display: 'flex', justifyContent: 'space-between' }}>
-            <Menu inverted>
-              {getAlgorithms(activeGroup).map(algorithm =>
-                <Menu.Item key={algorithm} as='a' active={activeAlgorithm === algorithm} onClick={() => selectAlgorithm(algorithm)}>
-                  {algorithm}
-                </Menu.Item>)}
-            </Menu>
-            <Header as='h1' inverted color='grey' style={{marginTop: '0'}}>
-              NEuler
-            </Header>
-          </Segment>
-
-          <MainContent />
-        </div>
-      </Container>
-    )
-  }
-}
 
 // const mapStateToProps = state => ({
 //   limit: state.settings.limit
---
+++ src/components/NEuler.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { Container, Menu, Segment, Dimmer, Loader, Header } from "semantic-ui-react"
+
+import AlgorithmsGroupMenu from "./AlgorithmGroupsMenu"
+import { getAlgorithms } from "./algorithmsLibrary"
+import MainContent from './MainContent'
+
+
+class NEuler extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  state = {
+    status: 'groups',
+    content: 'centralities'
+  }
+
+  handleMenuClick (content) {
+    this.setState({content})
+  }
+
+  render() {
+    const { activeGroup, activeAlgorithm, selectAlgorithm } = this.props
+
+    return (
+      <Container fluid style={{ display: 'flex' }}>
+          <AlgorithmsGroupMenu/>
+        <div style={{width: '100%'}}>
+          <Segment basic inverted vertical={false}
+                   style={{ height: '5em', display: 'flex', justifyContent: 'space-between' }}>
+            <Menu inverted>
+              {getAlgorithms(activeGroup).map(algorithm =>
+                <Menu.Item key={algorithm} as='a' active={activeAlgorithm === algorithm} onClick={() => selectAlgorithm(algorithm)}>
+                  {algorithm}
+                </Menu.Item>)}
+            </Menu>
+            <Header as='h1' inverted color='grey' style={{marginTop: '0'}}>
+              NEuler
+            </Header>
+          </Segment>
+
+          <MainContent />
+        </div>
+      </Container>
+    )
+  }
+}
+
+export default NEuler
