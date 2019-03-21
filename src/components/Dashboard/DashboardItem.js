1dbfe823a40f0252e501447dbaa86be3558849a2
Irfan Nuri Karaca <irfannuri@gmail.com>
2019-03-21 17:17:04 +0000

    Render dashboard items as table

--- src/components/Dashboard/DashboardItem.js
+++ src/components/Dashboard/DashboardItem.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { Card, Loader } from "semantic-ui-react"
 import Chart from "../visualisation/Chart"
 
-export default ({ name, mode, result }) => <Card style={{width: '30em'}}>
+import CentralityResult from '../Centralities/CentralityResult'
+
+export default ({ startTime, name, mode, result }) => <Card style={{ width: '30em' }}>
   <Card.Content>
     <Card.Header>{name}</Card.Header>
   </Card.Content>
   <Card.Content>
     {result
-      ? <Chart style={{width: '30em', height: '25em'}} data={result.map(result => ({
+      ? (
+        mode === 'chart'
+          ? <Chart style={{ width: '30em', height: '25em' }} data={result.map(result => ({
         name: result.properties.name || 'Node',
         score: result.score
-      }))}/>
+          }))}/>
+          : <CentralityResult task={{ startTime, name, mode, result }}/>
+      )
       : <Loader active={true} inline='centered'>Fetching Data</Loader>
     }
   </Card.Content>
