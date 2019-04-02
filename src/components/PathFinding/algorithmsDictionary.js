6b830db7e687095b4824891082109cd648640aed
Mark Needham <m.h.needham@gmail.com>
2019-04-02 18:11:00 +0100

    all pairs shortest path

--- src/components/PathFinding/algorithmsDictionary.js
+++ src/components/PathFinding/algorithmsDictionary.js
@@ -1,24 +1,27 @@
 import React from "react"
-import {runAlgorithm} from "../../services/pathFinding"
+import {runAlgorithm, runAllPairsShortestPathAlgorithm, runStreamingAlgorithm} from "../../services/pathFinding"
 import {Card} from "semantic-ui-react/dist/commonjs/views/Card"
 import {pathFindingParams} from "../../services/queries";
 import PathFindingResult from "./PathFindingResult";
 import ShortestPathForm from "./ShortestPathForm";
 import AStarForm from "./AStarForm";
 import SingleSourceShortestPathForm from "./SingleSourceShortestPathForm";
+import AllPairsShortestPathForm from "./AllPairsShortestPathForm";
+import AllPairsShortestPathResult from "./AllPairsShortestPathResult";
 
 export default {
     algorithmList: [
         "Shortest Path",
         "A*",
-        "Single Source Shortest Path"
+        "Single Source Shortest Path",
+        "All Pairs Shortest Path"
         // "Balanced Triads"
     ],
     algorithmDefinitions: {
         "Shortest Path": {
             Form: ShortestPathForm,
             parametersBuilder: pathFindingParams,
-            service: runAlgorithm,
+            service: runStreamingAlgorithm,
             ResultView: PathFindingResult,
             parameters: {
                 nodeQuery: null,
@@ -41,7 +44,7 @@ RETURN algo.getNodeById(nodeId) AS node, cost`,
         "A*": {
             Form: AStarForm,
             parametersBuilder: pathFindingParams,
-            service: runAlgorithm,
+            service: runStreamingAlgorithm,
             ResultView: PathFindingResult,
             parameters: {
                 nodeQuery: null,
@@ -66,7 +69,7 @@ RETURN algo.getNodeById(nodeId) AS node, cost`,
         "Single Source Shortest Path": {
             Form: SingleSourceShortestPathForm,
             parametersBuilder: pathFindingParams,
-            service: runAlgorithm,
+            service: runStreamingAlgorithm,
             ResultView: PathFindingResult,
             parameters: {
                 nodeQuery: null,
@@ -85,7 +88,31 @@ RETURN algo.getNodeById(nodeId) AS node, cost
 LIMIT $limit`,
             storeQuery: ``,
             getFetchQuery: () => "",
-            description: `The A* algorithm improves on the classic Dijkstra algorithm. by using a heuristic that guides the paths taken.`
+            description: `The Single Source Shortest Path (SSSP) algorithm calculates the shortest (weighted) path from a node to all other nodes in the graph..`
+        },
+        "All Pairs Shortest Path": {
+            Form: AllPairsShortestPathForm,
+            parametersBuilder: pathFindingParams,
+            service: runAllPairsShortestPathAlgorithm,
+            ResultView: AllPairsShortestPathResult,
+            parameters: {
+                nodeQuery: null,
+                relationshipQuery: null,
+                direction: 'Both',
+                persist: false,
+                defaultValue: 0.99,
+                weightProperty: "weight",
+                concurrency: 8,
+                delta: 3.0
+            },
+            streamQuery: `WITH algo.getNodeById($startNodeId) AS start
+CALL algo.allShortestPaths.stream($config.weightProperty, $config)
+YIELD sourceNodeId, targetNodeId, distance AS cost
+RETURN algo.getNodeById(sourceNodeId) AS source, algo.getNodeById(targetNodeId) AS target, cost
+LIMIT $limit`,
+            storeQuery: ``,
+            getFetchQuery: () => "",
+            description: `The All Pairs Shortest Path (APSP) calculates the shortest (weighted) path between all pairs of nodes.`
         },
     }
 }
--- src/services/pathFinding.js
+++ src/services/pathFinding.js
@@ -17,10 +17,32 @@ export const runAlgorithm = ({streamCypher, storeCypher, fetchCypher, parameters
     }
 }
 
-const runStreamingAlgorithm = (streamCypher, parameters, parseResultStreamFn=parseResultStream) => {
+export const runStreamingAlgorithm = (streamCypher, parameters, parseResultStreamFn=parseResultStream) => {
     return runCypher(streamCypher, parameters)
         .then(result => parseResultStreamFn(result))
         .catch(handleException)
+}
+
+export const runAllPairsShortestPathAlgorithm = ({streamCypher, parameters}) => {
+    return runStreamingAlgorithm(streamCypher, parameters, result => {
+        if (result.records) {
+            return result.records.map(record => {
+                const source = record.get('source')
+                const target = record.get('target')
+
+                return {
+                    sourceProperties: parseProperties(source.properties),
+                    sourceLabels: source.labels,
+                    targetProperties: parseProperties(target.properties),
+                    targetLabels: target.labels,
+                    cost: record.get('cost')
+                }
+            })
+        } else {
+            console.error(result.error)
+            throw new Error(result.error)
+        }
+    })
 }
 
 
