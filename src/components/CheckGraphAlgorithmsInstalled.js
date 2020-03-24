3a74e9975f19c7501cae7b3e070a358604b243f4
Mark Needham <m.h.needham@gmail.com>
2020-03-24 08:36:18 +0000

    add image showing how to install the plugin

--- src/components/CheckGraphAlgorithmsInstalled.js
+++ src/components/CheckGraphAlgorithmsInstalled.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Menu, Segment, Dimmer, Loader, Header } from "semantic-ui-react"
+import {Container, Menu, Segment, Dimmer, Loader, Header, Image} from "semantic-ui-react"
 
 import {checkGraphAlgorithmsInstalled} from "../services/installation"
 
@@ -22,7 +22,17 @@ class CheckGraphAlgorithmsInstalled extends Component {
       return this.props.children;
      } else {
        return <Dimmer active>
-         <Loader size='massive'>This application relies on the Graph Data Science Library. You can install it via the 'Plugins' tab in the project view.</Loader>
+         <Loader size='massive'>
+           <div align="center">
+             <p>This application relies on the Graph Data Science Library.
+             <br />You can install it via the 'Plugins' tab in the project view.</p>
+           </div>
+           <div align="center" style={{margin: "10px"}}>
+           <Image src='images/gds-plugin.png' size='big' />
+
+           </div>
+         </Loader>
+
        </Dimmer>
      }
   }
