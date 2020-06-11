8798b17002a20c99d18ffc3679a2e5fefc19d419
Mark Needham <m.h.needham@gmail.com>
2020-06-11 16:33:47 +0100

    bump verson

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,13 @@ import {Container, Message} from "semantic-ui-react"
 import React, {Component} from 'react'
 import {connect} from "react-redux";
 
-
 class About extends Component {
     render() {
         const containerStyle = {
             margin: "1em",
         }
+
+        const version = "0.1.20"
 
         console.log(this.props.metadata)
 
@@ -19,12 +20,18 @@ class About extends Component {
 
 
                         <p>
-                            Neo4j Server: <strong>{this.props.metadata.versions.neo4jVersion}</strong>
+                            NEuler: <strong>{version}</strong>
                         </p>
 
                         <p>
                             Graph Data Science Library: <strong>{this.props.metadata.versions.gdsVersion}</strong>
                         </p>
+
+                        <p>
+                            Neo4j Server: <strong>{this.props.metadata.versions.neo4jVersion}</strong>
+                        </p>
+
+
                     </Message>
 
                 </Container>
