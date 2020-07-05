097e5760d91ab0a545ee2878c23d25855b9bb410
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2020-07-05 11:29:11 +0200

    Catch constraint already exists

--- src/components/Datasets.js
+++ src/components/Datasets.js
@@ -43,7 +43,8 @@ class Datasets extends Component {
         queries.reduce((promiseChain, query, qIndex) => {
             return promiseChain.then(chainResults => {
                   this.setState({ currentQueryIndex: qIndex })
-                  return runCypher(query).then(currentResult => {
+                  return runCypher(query).catch(error => {console.log(error)})
+                  .then(currentResult => {
                       this.setState({
                           completedQueryIndexes: { ...this.state.completedQueryIndexes, [qIndex]: true }
                       })
