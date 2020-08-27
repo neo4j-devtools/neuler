91cbb75c435c93f5393e6bb7d26218d2c6082e3f
Mark Needham <m.h.needham@gmail.com>
2020-08-27 15:26:36 +0100

    show what's in the db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,19 @@
-import {
-  Button,
-  Card,
-  CardGroup,
-  Checkbox,
-  Container,
-  Divider,
-  Dropdown,
-  Form,
-  Header,
-  Icon,
-  TextArea
-} from "semantic-ui-react"
-import React, {Component, useState} from 'react'
+import {Message, Loader, Dimmer, Button, Card, CardGroup, Container, Divider, Dropdown, Header, Icon, Table} from "semantic-ui-react"
+import React, {Component} from 'react'
 
 import {selectGroup} from "../ducks/algorithms"
 import {connect} from "react-redux";
 import {setActiveDatabase, setDatabases, setLabels, setPropertyKeys, setRelationshipTypes} from "../ducks/metadata";
 import {getDriver, hasNamedDatabase, onActiveDatabase} from "../services/stores/neoStore";
 import {loadMetadata} from "../services/metadata";
-import {FeedbackForm} from "./Feedback/FeedbackForm";
 
 class Home extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            serverInfo: ""
+            serverInfo: "",
+            activeDatabaseSelected: true
         }
     }
 
@@ -36,6 +24,29 @@ class Home extends Component {
             this.props.setPropertyKeys(metadata.propertyKeys)
             this.props.setDatabases(metadata.databases)
         })
+    }
+
+    whatIsMissing(metadata) {
+        const hasNodeLabels = metadata.labels.length > 0;
+        const hasRelationshipTypes = metadata.relationshipTypes.length > 0;
+
+        if(!hasNodeLabels && !hasRelationshipTypes) {
+            return "nodes or relationships"
+        }
+
+        if(!hasNodeLabels) {
+            return "nodes";
+        }
+
+        if(!hasRelationshipTypes) {
+            return "relationships"
+        }
+
+        return null
+    }
+
+    hasNodesAndRelationships(metadata) {
+        return (metadata.labels.length > 0 && metadata.relationshipTypes.length > 0)
     }
 
     render() {
@@ -60,30 +71,90 @@ class Home extends Component {
                     <div>
                         <p>
                             Connected to: {this.state.serverInfo.address}
-
                         </p>
 
-                            <Button as='div' labelPosition='left'>
-                            <Dropdown value={metadata.activeDatabase} placeholder='Database' fluid search selection style={{"width": "290px"}}
+                        <Button as='div' labelPosition='left'>
+                            <Dropdown value={metadata.activeDatabase} placeholder='Database' fluid search selection
+                                      style={{"width": "290px"}}
                                       options={databaseOptions} onChange={(evt, data) => {
                                 setActiveDatabase(data.value);
                                 onActiveDatabase(data.value);
                                 this.onRefresh()
                             }}/>
+                            {hasNamedDatabase() ?
+                                <Button icon style={{marginLeft: "10px"}} onClick={ () => {
+                                    this.onRefresh.bind(this)
+                                }}>
+                                    <Icon className="refresh" size="large"/>
+                                </Button> : null}
+                        </Button>
+                    </div>
 
-                              {hasNamedDatabase() ? <Button icon style={{marginLeft: "10px"}} onClick={this.onRefresh.bind(this)}>
-                                <Icon className="refresh" size="large"  />
-                            </Button> : null}
-                            </Button>
+                    <div style={{paddingTop: "10px"}}>
+                        {
+                            this.state.activeDatabaseSelected ?
+                                (this.hasNodesAndRelationships(this.props.metadata)) ?
+                                    <div>
+                                        <p>
+                                            This database contains the following:
+                                        </p>
 
+                                        <Table basic='very' celled collapsing>
+                                            <Table.Body>
+                                                <Table.Row>
+                                                    <Table.Cell style={{width: "150px"}}>Node Labels</Table.Cell>
+                                                    <Table.Cell>{
+                                                        this.props.metadata.labels.map(value => <span
+                                                            className="label">{value.label}</span>)
 
+                                                    }
+                                                    </Table.Cell>
+                                                </Table.Row>
+                                                <Table.Row>
+                                                    <Table.Cell style={{width: "150px"}}>Relationship Types</Table.Cell>
+                                                    <Table.Cell>{
+                                                        this.props.metadata.relationshipTypes.map(value => <span
+                                                            className="label">{value.label}</span>)
+
+                                                    }
+                                                    </Table.Cell>
+                                                </Table.Row>
+                                            </Table.Body>
+                                        </Table>
+                                    </div>
+                                    :
+
+                                    <div>
+                                        <Message color='purple'>
+                                            <Message.Header>
+                                            This database does not contain any {this.whatIsMissing(this.props.metadata)}.
+                                            </Message.Header>
+                                            <Message.Content>
+                                                <div>
+                                                <p>
+                                                    You need to load some data, otherwise the graph algorithms won't return any results.
+                                                    If you don't have any data, you can load one of the sample graphs.
+                                                </p>
+
+                                                <Button primary onClick={() => selectGroup('Sample Graphs')}>
+                                                    View Sample Graphs
+                                                </Button>
+                                                </div>
+                                            </Message.Content>
+                                        </Message>
+
+                                    </div>
+
+                                :  <Loader active inline />
+
+                        }
 
                     </div>
 
                     <Divider />
 
                     <Header as={"h3"}>
-                        Algorithm Categories
+                        Getting Started
                     </Header>
                     <p>
                         The Neo4j Graph Data Science Library supports the following categories of algorithms.
