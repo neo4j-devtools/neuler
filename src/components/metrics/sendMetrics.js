20df3ff44b438ee4fc863d0a43df0bd007755418
Mark Needham <m.h.needham@gmail.com>
2020-09-01 17:32:56 +0100

    version 0.1.28

--- src/components/metrics/sendMetrics.js
+++ src/components/metrics/sendMetrics.js
@@ -1,5 +1,4 @@
 export const sendMetrics = (category, label, data = {}) => {
-    console.log(category, label, data)
     if (!!window.neo4jDesktopApi) {
         window.neo4jDesktopApi.sendMetrics(category, label, data)
     }
