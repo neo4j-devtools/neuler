2dce63468774ebd1e1e3c822c47ab0782c346fea
Mark Needham <m.h.needham@gmail.com>
2020-09-09 12:24:50 +0100

    scc

--- src/components/Communities/CommunityForm.js
+++ src/components/Communities/CommunityForm.js
@@ -1,40 +1,22 @@
 import React from 'react'
-import { Form, Input, Dropdown } from "semantic-ui-react"
+import {ProjectedGraphWithNoWeights} from "../Form/ProjectedGraph";
+import {ResultsStorage} from "../Form/ResultsStorage";
 
-export default ({onChange, relationshipType, direction, persist, writeProperty, labelOptions, relationshipTypeOptions, relationshipOrientationOptions}) => (
-  <React.Fragment>
-  <Form.Field>
-    <label>Label</label>
-    <Dropdown placeholder='Label' fluid search selection options={labelOptions} onChange={(evt, data) => onChange("label", data.value)} />
-  </Form.Field>
+const AlgoForm =  ({onChange, label, relationshipType, direction, persist, writeProperty, labelOptions, relationshipTypeOptions, relationshipOrientationOptions}) => {
+  const projectedGraphProps = {
+    label,
+    labelOptions,
+    relationshipType,
+    direction,
+    relationshipTypeOptions,
+    relationshipOrientationOptions,
+    onChange
+  }
 
-    <Form.Field>
-      <label>Relationship Type</label>
-      <Dropdown placeholder='RelationshipType' fluid search selection options={relationshipTypeOptions} onChange={(evt, data) => onChange("relationshipType", data.value)} />
-    </Form.Field>
-
-    {relationshipType ?
-      <Form.Field>
-        <label>Relationship Orientation</label>
-        <Dropdown placeholder='RelationshipOrientation' defaultValue={direction} fluid search selection options={relationshipOrientationOptions} onChange={(evt, data) => onChange("direction", data.value)} />
-      </Form.Field> : null }
-
-    <Form.Group inline>
-      <Form.Field inline>
-        <label style={{ 'width': '10em' }}>Store results</label>
-        <input type='checkbox' checked={persist} onChange={evt => {
-          console.log(evt.target, evt)
-          onChange('persist', evt.target.checked)
-        }}/>
-      </Form.Field>
-      {
-        persist ?
-          <Form.Field inline>
-            <Input size='mini' basic="true" value={writeProperty} placeholder='Write Property' onChange={evt => onChange('writeProperty', evt.target.value)}/>
-          </Form.Field>
-          : null
-      }
-    </Form.Group>
-
+  return <React.Fragment>
+    <ProjectedGraphWithNoWeights {...projectedGraphProps} />
+    <ResultsStorage persist={persist} onChange={onChange} writeProperty={writeProperty}/>
   </React.Fragment>
-)
+}
+
+export default AlgoForm
--- src/components/Communities/StronglyConnectedComponentsForm.js
+++ src/components/Communities/StronglyConnectedComponentsForm.js
@@ -2,18 +2,13 @@ import React, {Component} from 'react'
 import {Form} from "semantic-ui-react"
 import CommunityForm from './CommunityForm'
 
-export default class extends Component {
-  state = {
-    advanced: false
-  }
-
-  render() {
-    const { onChange, relationshipType, relationshipOrientationOptions, labelOptions, relationshipTypeOptions, writeProperty, direction, persist } = this.props
-
+const AlgoForm = ({ onChange, relationshipType, label, relationshipOrientationOptions, labelOptions, relationshipTypeOptions, writeProperty, direction, persist }) => {
     return (
       <Form size='mini' style={{ marginBottom: '1em' }}>
-        <CommunityForm relationshipOrientationOptions={relationshipOrientationOptions} relationshipType={relationshipType} onChange={onChange} direction={direction} persist={persist} writeProperty={writeProperty}  labelOptions={labelOptions} relationshipTypeOptions={relationshipTypeOptions}/>
+        <CommunityForm label={label} relationshipOrientationOptions={relationshipOrientationOptions} relationshipType={relationshipType} onChange={onChange} direction={direction} persist={persist} writeProperty={writeProperty}  labelOptions={labelOptions} relationshipTypeOptions={relationshipTypeOptions}/>
       </Form>
     )
-  }
+
 }
+
+export default AlgoForm
