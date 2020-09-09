09769e3af8e0e769cb196c74215118a83c26947c
Mark Needham <m.h.needham@gmail.com>
2020-09-09 11:03:24 +0100

    page rank with new sections

--- src/components/Centralities/DegreeForm.js
+++ src/components/Centralities/DegreeForm.js
@@ -3,22 +3,27 @@ import {Form, Input} from "semantic-ui-react"
 import {ProjectedGraph} from "../Form/ProjectedGraph";
 import {ResultsStorage} from "../Form/ResultsStorage";
 
-export default class extends Component {
-  state = {
-    advanced: false
-  }
-
-  render() {
-    const { label, relationshipType, labelOptions, relationshipTypeOptions, propertyKeyOptions, relationshipOrientationOptions, onChange, writeProperty, weightProperty, defaultValue, direction, persist } = this.props
-
-    const projectedGraphProps = {label, labelOptions, relationshipType, direction, relationshipTypeOptions, relationshipOrientationOptions, propertyKeyOptions, weightProperty, defaultValue,  onChange}
+const DegreeForm = (props) => {
+    const {label, relationshipType, labelOptions, relationshipTypeOptions, propertyKeyOptions, relationshipOrientationOptions, onChange, writeProperty, weightProperty, defaultValue, direction, persist} = props
+    const projectedGraphProps = {
+        label,
+        labelOptions,
+        relationshipType,
+        direction,
+        relationshipTypeOptions,
+        relationshipOrientationOptions,
+        propertyKeyOptions,
+        weightProperty,
+        defaultValue,
+        onChange
+    }
 
     return (
         <Form size='mini' style={{marginBottom: '1em'}}>
             <ProjectedGraph {...projectedGraphProps} />
-            <ResultsStorage persist={persist} onChange={onChange} writeProperty={writeProperty} />
+            <ResultsStorage persist={persist} onChange={onChange} writeProperty={writeProperty}/>
         </Form>
     )
-  }
 }
 
+export default DegreeForm
--- src/components/Centralities/PageRankForm.js
+++ src/components/Centralities/PageRankForm.js
@@ -1,87 +1,63 @@
 import React, {Component} from 'react'
-import {Dropdown, Form, Input} from "semantic-ui-react"
+import {Dropdown, Form, Input, Label, Segment} from "semantic-ui-react"
+import {ProjectedGraph} from "../Form/ProjectedGraph";
+import {ResultsStorage} from "../Form/ResultsStorage";
 
-export default class extends Component {
-  state = {
-    advanced: false
-  }
-
-  render() {
-    const { label, relationshipType, labelOptions, relationshipTypeOptions, relationshipOrientationOptions, propertyKeyOptions, writeProperty, onChange, maxIterations, dampingFactor, weightProperty, defaultValue, direction, persist } = this.props
+const PageRankForm = (props) => {
+    const {label, relationshipType, labelOptions, relationshipTypeOptions, relationshipOrientationOptions, propertyKeyOptions, writeProperty, onChange, maxIterations, dampingFactor, weightProperty, defaultValue, direction, persist} = props
+    const projectedGraphProps = {
+        label,
+        labelOptions,
+        relationshipType,
+        direction,
+        relationshipTypeOptions,
+        relationshipOrientationOptions,
+        propertyKeyOptions,
+        weightProperty,
+        defaultValue,
+        onChange
+    }
 
     return (
-      <Form size='mini' style={{ marginBottom: '1em' }}>
-        <Form.Field>
-          <label>Label</label>
-          <Dropdown placeholder='Label' value={label} fluid search selection options={labelOptions} onChange={(evt, data) => onChange("label", data.value)} />
-        </Form.Field>
-        <Form.Field>
-          <label>Relationship Type</label>
-          <Dropdown placeholder='RelationshipType' value={relationshipType} fluid search selection options={relationshipTypeOptions} onChange={(evt, data) => onChange("relationshipType", data.value)} />
-        </Form.Field>
+        <Form size='mini' style={{marginBottom: '1em'}}>
+            <ProjectedGraph {...projectedGraphProps} />
+            <PageRankParameters maxIterations={maxIterations} dampingFactor={dampingFactor} onChange={onChange}/>
+            <ResultsStorage persist={persist} onChange={onChange} writeProperty={writeProperty}/>
 
-        {relationshipType ?
-          <Form.Field>
-            <label>Relationship Orientation</label>
-            <Dropdown placeholder='RelationshipOrientation' defaultValue={direction} fluid search selection options={relationshipOrientationOptions} onChange={(evt, data) => onChange("direction", data.value)} />
-          </Form.Field> : null }
-
-
-        {relationshipType ?
-          <Form.Field inline>
-            <label style={{ 'width': '8em' }}>Weight Property</label>
-            <Dropdown placeholder='Weight Property' defaultValue={weightProperty} fluid search selection options={propertyKeyOptions} onChange={(evt, data) => onChange("weightProperty", data.value)} />
-          </Form.Field> : null }
-        {
-          weightProperty ?
-            <Form.Field inline>
-              <label style={{ 'width': '8em' }}>Default weight</label>
-              <input
-                value={defaultValue}
-                onChange={evt => onChange('defaultValue', evt.target.value)}
-                style={{ 'width': '7em' }}
-              />
-            </Form.Field>
-            : null
-        }
-
-        <Form.Group inline>
-          <Form.Field inline>
-            <label style={{ 'width': '8em' }}>Store results</label>
-            <input type='checkbox' checked={persist} onChange={evt => {
-              onChange('persist', evt.target.checked)
-            }}/>
-          </Form.Field>
-          {
-            persist ?
-              <Form.Field inline>
-                <Input size='mini' basic="true" value={writeProperty} placeholder='Write Property' onChange={evt => onChange('writeProperty', evt.target.value)}/>
-              </Form.Field>
-              : null
-          }
-        </Form.Group>
-
-        <Form.Field inline>
-          <label style={{ 'width': '8em' }}>Iterations</label>
-          <input
-            type='number'
-            min={1}
-            max={50}
-            step={1}
-            value={maxIterations}
-            onChange={evt => onChange('maxIterations', evt.target.value)}
-            style={{ 'width': '5em' }}
-          />
-        </Form.Field>
-        <Form.Field inline>
-          <label style={{ 'width': '8em' }}>Damping Factor</label>
-          <input
-            value={dampingFactor}
-            onChange={evt => onChange('dampingFactor', evt.target.value)}
-            style={{ 'width': '5em' }}
-          />
-        </Form.Field>
-      </Form>
+        </Form>
     )
-  }
 }
+
+
+
+const PageRankParameters = ({maxIterations, dampingFactor, onChange}) => {
+    return <Segment key={maxIterations}>
+        <Label as='a' attached='top left'>
+            Algorithm Parameters
+        </Label>
+        <Form.Field inline>
+            <label style={{'width': '8em'}}>Iterations</label>
+            <input
+                key={maxIterations}
+                type='number'
+                min={1}
+                max={50}
+                step={1}
+                value={maxIterations}
+                onChange={evt => onChange('maxIterations', evt.target.value)}
+                style={{'width': '5em'}}
+            />
+        </Form.Field>
+        <Form.Field inline>
+            <label style={{'width': '8em'}}>Damping Factor</label>
+            <input
+                key={dampingFactor}
+                value={dampingFactor}
+                onChange={evt => onChange('dampingFactor', evt.target.value)}
+                style={{'width': '5em'}}
+            />
+        </Form.Field>
+    </Segment>
+}
+
+export default PageRankForm
