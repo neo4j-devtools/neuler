478e1bd2ebbfcce29ac4b2e22ccab193e05b7bbb
Mark Needham <m.h.needham@gmail.com>
2020-09-10 12:25:26 +0100

    pull out results filtering fields

--- src/components/Form/ResultsFiltering.js
+++ src/components/Form/ResultsFiltering.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Form, Input, Label, Segment} from "semantic-ui-react";
 
-export const ResultsFiltering = ({limit, communityNodeLimit, returnsCommunities, updateCommunityNodeLimit, updateLimit, readOnly}) => {
+export const ResultsFiltering = (props) => {
     const [open, setOpen] = React.useState(true);
     const style = {display: open ? "" : "none"}
 
@@ -12,16 +12,22 @@ export const ResultsFiltering = ({limit, communityNodeLimit, returnsCommunities,
 
 
         <Form style={style}>
-            <Form.Field disabled={readOnly} inline label={<label style={{'width': '12em'}}>Rows to show</label>}
-                        control={Input} type='number' value={limit} onChange={updateLimit} min={1} max={1000} step={1}/>
-
-            {returnsCommunities ?
-                <Form.Field disabled={readOnly} inline
-                            label={<label style={{'width': '12em'}}>Community Node Limit</label>}
-                            control={Input} type='number' value={communityNodeLimit} onChange={updateCommunityNodeLimit}
-                            min={1} max={1000} step={1}/>
-                : null
-            }
+            <ResultFilteringFields {...props} />
         </Form>
     </Segment>
+}
+
+export const ResultFilteringFields = ({limit, communityNodeLimit, returnsCommunities, updateCommunityNodeLimit, updateLimit, readOnly}) => {
+    return <React.Fragment>
+        <Form.Field disabled={readOnly} inline label={<label style={{'width': '12em'}}>Rows to show</label>}
+                    control={Input} type='number' value={limit} onChange={updateLimit} min={1} max={1000} step={1}/>
+
+        {returnsCommunities ?
+            <Form.Field disabled={readOnly} inline
+                        label={<label style={{'width': '12em'}}>Community Node Limit</label>}
+                        control={Input} type='number' value={communityNodeLimit} onChange={updateCommunityNodeLimit}
+                        min={1} max={1000} step={1}/>
+            : null
+        }
+    </React.Fragment>
 }
