2f3449a2ae79705e6b90eb7206605f4199d327f2
Mark Needham <m.h.needham@gmail.com>
2020-09-10 16:34:20 +0100

    removing the old component

--- src/WebApp.js
+++ src/WebApp.js
@@ -67,12 +67,11 @@ const onConnected = (props) => {
     });
 }
 
+const steps = [
+    CONNECTING_TO_DATABASE, CHECKING_GDS_PLUGIN, CHECKING_APOC_PLUGIN, ALL_DONE
+]
 
 const NewApp = (props) => {
-    const steps = [
-        CONNECTING_TO_DATABASE, CHECKING_GDS_PLUGIN, CHECKING_APOC_PLUGIN, ALL_DONE
-    ]
-
     const [currentStep, setCurrentStep] = React.useState(CONNECTING_TO_DATABASE)
     const [currentStepFailed, setCurrentStepFailed] = React.useState(false)
     const [showNeuler, setShowNeuler] = React.useState(false)
@@ -150,15 +149,15 @@ const NewApp = (props) => {
                 <Divider/>
                 <div style={{textAlign: "center", paddingTop: "10px", display: "flex", justifyContent: "center"}}>
                     <div className="loading">
-                        <LoadingIcon step={CONNECTING_TO_DATABASE} steps={steps} currentStep={currentStep} currentStepFailed={currentStepFailed} />
+                        <LoadingIcon step={CONNECTING_TO_DATABASE} currentStep={currentStep} currentStepFailed={currentStepFailed} />
                         <p>Connecting to database</p>
                     </div>
                     <div className="loading">
-                        <LoadingIcon step={CHECKING_GDS_PLUGIN} steps={steps} currentStep={currentStep} currentStepFailed={currentStepFailed} />
+                        <LoadingIcon step={CHECKING_GDS_PLUGIN} currentStep={currentStep} currentStepFailed={currentStepFailed} />
                         <p>Checking GDS plugin</p>
                     </div>
                     <div className="loading">
-                        <LoadingIcon step={CHECKING_APOC_PLUGIN} steps={steps} currentStep={currentStep} currentStepFailed={currentStepFailed} />
+                        <LoadingIcon step={CHECKING_APOC_PLUGIN} currentStep={currentStep} currentStepFailed={currentStepFailed} />
                         <p>Checking APOC plugin</p>
                     </div>
                 </div>
@@ -211,7 +210,7 @@ const ConnectingToDatabase = ({connectionStatus, setCurrentStep, setConnected, s
 
 }
 
-const LoadingIcon = ({step, steps, currentStep, currentStepFailed}) => {
+const LoadingIcon = ({step, currentStep, currentStepFailed}) => {
     const currentIndex = steps.indexOf(currentStep)
     const stepIndex = steps.indexOf(step)
 
@@ -226,228 +225,6 @@ const LoadingIcon = ({step, steps, currentStep, currentStepFailed}) => {
             return <Icon size="big" name='checkmark' color='green' className="loading-icon" />
         }
     }
-}
-
-class App extends Component {
-  steps = [
-      CONNECTING_TO_DATABASE, CHECKING_GDS_PLUGIN, CHECKING_APOC_PLUGIN, ALL_DONE
-  ]
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      errorMsg: "Could not get a connection! Check that you entered the correct credentials and that the database is running.",
-      currentStep: CONNECTING_TO_DATABASE,
-      currentStepFailed: false
-    }
-
-    const { setConnected, setDisconnected } = this.props
-
-    initializeConnection(setConnected, setDisconnected, (error) => this.setState({errorMsg: error}))
-  }
-
-  componentDidMount() {
-    if (this.props.connectionInfo.status === CONNECTED) {
-      this.onConnected()
-    }
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
-    if (this.props.connectionInfo !== nextProps.connectionInfo
-      && nextProps.connectionInfo.status === CONNECTED) {
-      this.setState({
-        currentStep: CHECKING_GDS_PLUGIN,
-        currentStepFailed: false
-      })
-      this.onConnected()
-    }
-  }
-
-  onConnected() {
-    checkGraphAlgorithmsInstalled().then((gdsInstalled) => {
-      checkApocInstalled().then(apocInstalled => {
-        if (apocInstalled && gdsInstalled) {
-          loadVersions().then(versions => {
-            sendMetrics("neuler-connected", true, versions)
-
-            this.props.setGds(versions)
-            onNeo4jVersion(versions.neo4jVersion)
-            loadMetadata(versions.neo4jVersion).then(metadata => {
-              this.props.setLabels(metadata.labels)
-              this.props.setRelationshipTypes(metadata.relationships)
-              this.props.setPropertyKeys(metadata.propertyKeys)
-              this.props.setNodePropertyKeys(metadata.nodePropertyKeys)
-              this.props.setDatabases(metadata.databases)
-
-              metadata.databases.forEach(database => {
-                this.props.addDatabase(database.name)
-              })
-
-              metadata.labels.forEach(label => {
-                this.props.initLabel(this.props.metadata.activeDatabase, label.label, selectRandomColor(), selectCaption(metadata.nodePropertyKeys[label.label]))
-              })
-
-            })
-          });
-        } else {
-          sendMetrics("neuler", "neuler-connected-incomplete", {gdsInstalled, apocInstalled})
-        }
-      })
-    });
-  }
-
-  renderIcon(step) {
-    const {currentStep, currentStepFailed} = this.state
-
-    const currentIndex = this.steps.indexOf(currentStep)
-    const stepIndex = this.steps.indexOf(step)
-
-    if(step === this.state.currentStep) {
-      return currentStepFailed ?
-          <LoadingIcon size="big" name='close' color='red' className="loading-icon" /> :
-          <Loader active inline  className="loading-icon" size="large" />;
-    } else {
-      if(stepIndex > currentIndex) {
-        return <LoadingIcon size="big" name='circle notch' color='grey' className="loading-icon" />
-      } else {
-        return <LoadingIcon size="big" name='checkmark' color='green' className="loading-icon" />
-      }
-    }
-  }
-
-  renderExtra(connectionStatus) {
-    const {setConnected} = this.props
-    const {currentStep} = this.state
-
-    const placeholder =
-      <Loader size='massive'>Checking plugin is installed</Loader>
-
-    const tryingToConnect = <div style={{padding: "20px"}}>
-      <Message grey attached header="Trying to connect" content="Trying to connect to active database. This should only take a few seconds. If it takes longer than that, check that you have a running database."/>
-    </div>
-
-      switch (currentStep) {
-          case CONNECTING_TO_DATABASE:
-              switch (connectionStatus) {
-                  case INITIAL:
-                  case DISCONNECTED:
-                      return <ConnectModal
-                          key="modal"
-                          errorMsg={this.state.errorMsg}
-                          onSubmit={(username, password) => {
-                              const credentials = {username, password}
-                              tryConnect(credentials)
-                                  .then(() => {
-                                      this.setState({
-                                          currentStep: CHECKING_GDS_PLUGIN
-                                      })
-                                      setConnected(credentials)
-                                  })
-                                  .catch(setDisconnected)
-                          }}
-                          show={true}
-                      />
-
-                  case CONNECTING:
-                      return tryingToConnect
-                  default:
-                      return tryingToConnect
-              }
-          case CHECKING_GDS_PLUGIN:
-              return <CheckGraphAlgorithmsInstalled
-                  didNotFindPlugin={this.failedCurrentStep.bind(this)}
-                  gdsInstalled={this.gdsInstalled.bind(this)}>
-                  {placeholder}
-              </CheckGraphAlgorithmsInstalled>;
-          case CHECKING_APOC_PLUGIN:
-              return <CheckAPOCInstalled
-                  didNotFindPlugin={this.failedCurrentStep.bind(this)}
-                  apocInstalled={this.apocInstalled.bind(this)}>
-                  {placeholder}
-              </CheckAPOCInstalled>;
-          case ALL_DONE:
-              return <div style={{padding: "20px"}}>
-                  <Message color="grey" attached header="Neuler ready to launch"
-                           content="Connected to active database and all dependencies found. Neuler will launch shortly"/>
-              </div>
-          default:
-              return <Message>Unknown State</Message>;
-      }
-  }
-
-  failedCurrentStep(library) {
-    this.setState(
-        { currentStepFailed: true}
-    )
-  }
-
-  gdsInstalled() {
-    this.setState({
-      currentStepFailed: false,
-      currentStep: CHECKING_APOC_PLUGIN
-    })
-  }
-
-  apocInstalled() {
-    this.setState({
-      currentStepFailed: false,
-      currentStep: ALL_DONE
-    })
-  }
-
-
-  render() {
-    const {currentStep, showNeuler} = this.state
-
-    if(currentStep === ALL_DONE) {
-      if(showNeuler) {
-        return <NEuler key="app" {...this.props} />;
-      } else {
-        const that = this;
-        setTimeout(function () {
-          that.setState({
-            showNeuler: true
-          });
-        }, 1500);
-      }
-    }
-
-    const {  connectionInfo } = this.props
-
-    const extra = this.renderExtra(connectionInfo.status)
-
-    return <Container fluid style={{display: 'flex'}}>
-      <div style={{width: '100%'}}>
-        <Segment basic inverted vertical={false} style={{height: '100vh'}}>
-          <div style={{textAlign: "center"}}>
-            <h1 className="loading">Launching NEuler - The Graph Data Science Playground</h1>
-          </div>
-          <Divider/>
-          <div style={{textAlign: "center", paddingTop: "10px", display: "flex", justifyContent: "center"}}>
-            <div className="loading">
-              {this.renderIcon(CONNECTING_TO_DATABASE)}
-              <p>Connecting to database</p>
-            </div>
-            <div className="loading">
-              {this.renderIcon(CHECKING_GDS_PLUGIN)}
-
-              <p>Checking GDS plugin</p>
-            </div>
-            <div className="loading">
-              {this.renderIcon(CHECKING_APOC_PLUGIN)}
-              <p>Checking APOC plugin</p>
-            </div>
-          </div>
-
-          <div style={{textAlign: "center"}}>
-            {extra}
-          </div>
-
-        </Segment>
-      </div>
-    </Container>
-  }
 }
 
 const mapStateToProps = state => ({
