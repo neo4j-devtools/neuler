662b01604867a37760dcda9e5d74898a734eb89c
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2021-05-24 11:03:26 +0200

    Update node2vec to support 1.6

--- src/components/GraphEmbeddings/algorithmsDictionary.js
+++ src/components/GraphEmbeddings/algorithmsDictionary.js
@@ -3,6 +3,7 @@ import {embeddingParams} from "../../services/queries";
 import Result from "./Result";
 import Node2VecForm_1Point3 from "./Node2Vec/1.3/Form";
 import Node2VecForm_1Point4 from "./Node2Vec/1.4/Form";
+import Node2VecForm_1Point6 from "./Node2Vec/1.6/Form";
 import FastRPForm_1Point4 from "./FastRP/1.4/Form";
 import FastRPForm_1Point3 from "./FastRP/1.3/Form";
 
@@ -19,6 +20,42 @@ const commonRelWeightParameters = {
         relationshipWeightProperty: null,
     }
 }
+
+const node2Vec1_6 = {
+    "Node2Vec": {
+        algorithmName: "gds.beta.node2vec",
+        Form: Node2VecForm_1Point6,
+        parametersBuilder: embeddingParams,
+        service: runAlgorithm,
+        ResultView: Result,
+        parameters: {
+            ...commonRelWeightParameters, ...{
+                writeProperty: "node2Vec",
+                embeddingDimension: 10,
+                iterations: 1,
+                walkLength: 80,
+                inOutFactor: 1.0,
+                returnFactor: 1.0,
+                windowSize: 10
+            }
+        },
+        streamQuery: `CALL gds.beta.node2vec.stream($config)
+YIELD nodeId, embedding
+RETURN gds.util.asNode(nodeId) AS node, embedding
+LIMIT toInteger($limit)`,
+        storeQuery: `CALL gds.beta.node2vec.write($config)`,
+        getFetchQuery: (label, config) => {
+            const escapedLabel = config.nodeProjection && config.nodeProjection !== "*" ? ":`" + config.nodeProjection + "`" : ""
+            return `MATCH (node${escapedLabel})
+WHERE exists(node.\`${config.writeProperty}\`)
+WITH node, node.\`${config.writeProperty}\` AS embedding
+WITH node, CASE WHEN apoc.meta.type(embedding) = "float[]" THEN embedding ELSE null END as embedding
+RETURN node, embedding
+LIMIT toInteger($limit)`
+        },
+        description: `computes a vector representation of a node based on random walks in the graph`,
+    },
+};
 
 const node2Vec1_4 = {
     "Node2Vec": {
@@ -163,7 +200,7 @@ export default {
     },
     algorithmDefinitions: (algorithm, gdsVersion) => {
         const version = parseInt(gdsVersion.split(".")[1])
-        const algorithms = version >= 4 ? {...node2Vec1_4, ...fastRP1_4} : {...node2Vec1_3, ...fastRP1_3};
+        const algorithms = version >= 6 ? {...node2Vec1_6, ...fastRP1_4} : version >= 4 ? {...node2Vec1_4, ...fastRP1_4} : {...node2Vec1_3, ...fastRP1_3};
         return algorithms[algorithm]
 
     },
